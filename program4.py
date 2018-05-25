350532e99540ce93c5e3fdfaafc4b1345db66881
Jamie <jamieefung@gmail.com>
2018-05-25 08:11:26 -0400

    fixed the p_list not updating correctly

--- program4.py
+++ program4.py
@@ -6,15 +6,15 @@ learning_rate = 0
 train_data = [eight,enter,man,destruction,to_assemble,perish]
 labels = ["eight", "enter", "man", "destruction", "to assemble", "perish"]
 
-for i in range(1,11):
-	learning_rate = i/10
+# for i in range(1,11):
+# 	learning_rate = i/10
 
-	print("learning rate:", learning_rate)
-	p = Perceptron(eta0=learning_rate)
-	p.fit(train_data, labels)
+# 	print("learning rate:", learning_rate)
+# 	p = Perceptron(eta0=learning_rate)
+# 	p.fit(train_data, labels)
 
-	a = p.predict(train_data)
-	print(a)
+# 	a = p.predict(train_data)
+# 	print(a)
 
 
 # ------------ TRAINING A CUSTOM MADE PERCEPTRON --------------
@@ -26,16 +26,16 @@ y = 0
 a = .1 
 theta = 10
 iterations = 5 
-p_list = [] #store list of perceptrons
+p_list = [0,0,0,0,0,0] #store list of perceptrons
 
 # train perceptrons to recognize characters
-for alpha in range(1,11):
+for alpha in range(0,3):
 	a = alpha/10
 	print("\n-------- ALPHA", a, "--------")
 	for i in range(len(x)):
 		temp_p = Custom_perceptron(x,t_list[i],a,theta)
 		temp_p.train(iterations)
-		p_list.append(temp_p)
+		p_list[i] = temp_p
 
 	for i in range(len(x)):
 		print(labels[i], "perceptron")
